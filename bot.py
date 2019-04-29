42c7386b67fd07f60ed5310562498b38387e0038
kazu20 <kazu.ipod.touch.07@gmail.com>
2019-04-29 17:29:23 +0900

    sentimentをdatastoreに保存する処理を追加

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
 # Imports the Google Cloud client library
+from google.cloud import datastore
 from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
 from flask import Flask, request, abort
 
-#Imports LineBot
+# Imports LineBot
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -12,7 +13,8 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, ImageMessage, StickerMessage,
+    MessageEvent, TextMessage, TextSendMessage,
+    ImageMessage, StickerMessage,
 )
 import os
 
@@ -21,6 +23,7 @@ line_bot_api = LineBotApi(os.environ.get('LINE_TOKEN'))
 handler = WebhookHandler(os.environ.get('LINE_CHANNEL_SECRET'))
 
 app = Flask(__name__)
+
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -51,10 +54,28 @@ def handle_message(event):
         content=event.message.text,
         type=enums.Document.Type.PLAIN_TEXT)
 
+    # set profile
+    userId = event.source.user_id
+    profile = line_bot_api.get_profile(userId)
+    timestamp = event.timestamp
+
     # Detects the sentiment of the text
     sentiment = client.analyze_sentiment(document=document).document_sentiment
 
-    #
+    # Create Datastore Entity
+    datastore_client = datastore.Client()
+    kind = 'Sentiment'
+    task_key = datastore_client.key(kind)
+
+    Sentiment = datastore.Entity(key=task_key)
+    Sentiment['sentiment'] = sentiment.score
+    Sentiment['userId'] = userId
+    Sentiment['user_display_name'] = profile.display_name
+    Sentiment['timestamp'] = timestamp
+
+    datastore_client.put(Sentiment)
+
+    # メッセージのsentimentに合わせて、応答を返す
     if sentiment.score > 0.8:
         text = "激嬉"
     elif sentiment.score > 0.3:
@@ -68,11 +89,13 @@ def handle_message(event):
         event.reply_token,
         TextSendMessage(text=text))
 
+
 @handler.add(MessageEvent, message=StickerMessage)
 def handle_sticker(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text="スタンプはスタンプは使えません"))
+
 
 @handler.default()
 def default(event):
@@ -80,6 +103,6 @@ def default(event):
         event.reply_token,
         TextSendMessage(text="テキストメッセージを送ってください。"))
 
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
-
